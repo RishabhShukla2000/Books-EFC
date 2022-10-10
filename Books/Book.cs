49b4d518678705081727a9c98658d012eb4ec89d
Rishabh Shukla <43636003+RishabhShukla2000@users.noreply.github.com>
2022-10-10 12:58:56 +0530

    Delete Books directory

--- Books/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Books
-{
-    public class Book
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = String.Empty;
-        public string Writer { get; set; } = String.Empty;
-    }
-}
