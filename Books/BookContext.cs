0da75825fe59c41a12833d49e4ac843db9852c8e
Rishabh Shukla <43636003+RishabhShukla2000@users.noreply.github.com>
2022-10-10 13:01:13 +0530

    Add files via upload

---
+++ Books/BookContext.cs
@@ -0,0 +1,12 @@
+﻿global using Microsoft.EntityFrameworkCore;
+namespace Books
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books => Set<Book>();
+    }
+}
---
+++ Books/Writer.cs
@@ -0,0 +1,11 @@
+﻿namespace Books
+{
+    public class Writer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
