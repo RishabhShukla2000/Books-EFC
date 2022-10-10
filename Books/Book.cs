7201b54a1d434fd7647a6903b6c8ca3d4529479e
Rishabh Shukla <43636003+RishabhShukla2000@users.noreply.github.com>
2022-10-10 10:51:26 +0530

    Add files via upload

---
+++ Books/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Books
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Writer { get; set; } = String.Empty;
+    }
+}
